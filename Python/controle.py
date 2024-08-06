26d161ec8470a157f31efb1439aab8663753c929
MatheusOliveiraT <15matheusdeoliveira@gmail.com>
2024-08-05 21:06:01 -0300

    Definição classes Python
    Definição das primeiras classes do projeto em Python

---
+++ Python/controle.py
@@ -0,0 +1,20 @@
+from classes import *
+
+class TurmaDAO:
+    def __init__(self):
+        pass
+
+    def create_turma():
+        pass
+
+    def retrive_turma():
+        pass
+
+    def update_turma():
+        pass
+
+    def delete_turma():
+        pass
+
+    def query_turma():
+        pass
