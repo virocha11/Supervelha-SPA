80ee75451de8d59bf95b96348bc392a43ef1cc2f
Matheus de Oliveira Teixeira <135910565+MatheusOliveiraT@users.noreply.github.com>
2024-08-19 00:37:17 -0300

    Cadastro Professor e Aluno
    Lógica back-end de cadastro de professor e aluno, com inclusão de sessão e autenticação.

---
+++ back_end/cadastro/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.models import User, Group
+import re
+
+# Create your views here.
+
+def home(request: HttpRequest):
+    return render(request, 'registration/home.html')
+
+def cadastro_aluno(request: HttpRequest):
+    return render(request, 'registration/aluno.html')
+
+def cadastro_professor(request: HttpRequest):
+    return render(request, 'registration/professor.html')
+
+def validar_cadastro(request: HttpRequest):
+    if request.POST.get('password') != request.POST.get('cpassword'):
+        return False, HttpResponse('As duas senhas digitadas são diferentes.')
+    r = re.compile(r'^[\w-]+@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$')
+    if (not r.match(request.POST.get('email'))):
+        return False, HttpResponse('E-mail inválido.')
+    usuarios = User.objects.all()
+    for usuario in usuarios:
+        if usuario.email == request.POST.get('email'):
+            return False, HttpResponse('E-mail já cadastrado.')
+        if usuario.username == request.POST.get('username'):
+            return False, HttpResponse('Nome de usuário em uso.')
+    return True
+
+def cadastrar_aluno(request: HttpRequest):
+    if (validar_cadastro(request) == True):
+        novo_aluno = User.objects.create_user(request.POST.get('username'), request.POST.get('email'), request.POST.get('password'))
+        grupo = Group.objects.get(pk = 1)
+        novo_aluno.groups.add(grupo)
+        return HttpResponse(f'Cadastrado com sucesso! Seu ID é: {novo_aluno.id}')
+    else:
+        return(validar_cadastro(request)[1])
+
+def cadastrar_professor(request: HttpRequest):
+    if (validar_cadastro(request) == True):
+        novo_professor = User.objects.create_user(request.POST.get('username'), request.POST.get('email'), request.POST.get('password'))
+        grupo = Group.objects.get(pk = 2)
+        novo_professor.groups.add(grupo)
+        return HttpResponse(f'Cadastrado com sucesso! Seu ID é: {novo_professor.id}')
+    else:
+        return(validar_cadastro(request)[1])
---
+++ back_end/supervelha/settings.py
@@ -0,0 +1,102 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-kfs)$@c8hph&)$%e_97)pb490h69jdjs-ka2dqfh0(-wx*ga*#'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'debug_toolbar',
+]
+
+MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ROOT_URLCONF = 'supervelha.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                
+            ],
+        },
+    },
+]
+
+LOGIN_REDIRECT_URL = '/'
+
+WSGI_APPLICATION = 'supervelha.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'supervelha',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Sao_Paulo'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
