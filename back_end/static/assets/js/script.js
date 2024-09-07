ab0106ae193d56adcc9cd7f6d1f8e28fe7fe0214
taisbarbosa <taisbarbosarodrigues2004@gmail.com>
2024-09-07 00:29:58 -0300

    nova f: voltar ao topo; rolagem do modal; trocar senha no perfil( mudancas em html, css e js); padronizacao detalhes

--- back_end/static/assets/js/script.js
+++ back_end/static/assets/js/script.js
@@ -6,6 +6,7 @@ function abrir_modal(carregar_modal){
     modal.style.display = 'block';
     document.body.style.overflow = 'hidden';
 
+
 }
 function fechar_modal(fechar_modal){
     console.log("Fechar a janela modal: "+ fechar_modal);
@@ -16,3 +17,55 @@ function fechar_modal(fechar_modal){
 }
 // FIM FUNCOES ABRIR E FECHAR MODAL
 
+//DEFININDO ROLAGEM DO SCROLL PARA APLICAR A FUNCAO DE VOLTAR AO TOPO DA PAGINA
+window.addEventListener('scroll', function(){
+    let scroll = document.querySelector('.rolar_cima')
+    scroll.classList.toggle('active', window.scrollY > 300)
+})
+
+//FUNCAO DE VOLTAR AO TOPO DA PAGINA
+function voltar_cima(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+
+
+//HABILITANDO E DESABILITANDO OS CAMPOS DE TROCAS SENHA QUE ESTAO NO PERFIL DO USUARIO
+//PARA EVITAR ERROS AO ENVIAR O FORMULARIO SEM TROCAR A SENHA
+
+document.getElementById('trocar_senha').addEventListener('click', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+
+    var senha = document.getElementById('senha');
+    if (senha.classList.contains('inativo')) {
+        senha.classList.remove('inativo');
+        senha.classList.add('ativo');
+        senha.disabled = false;
+    } else {
+        senha.classList.remove('ativo');
+        senha.classList.add('inativo');
+        senha.disabled = true;
+    }    
+    senha = document.getElementById('nova_senha');
+    if (senha.classList.contains('inativo')) {
+        senha.classList.remove('inativo');
+        senha.classList.add('ativo');
+        senha.disabled = false;
+    } else {
+        senha.classList.remove('ativo');
+        senha.classList.add('inativo');
+        senha.disabled = true;
+    }    
+    senha = document.getElementById('confirma_senha');
+    if (senha.classList.contains('inativo')) {
+        senha.classList.remove('inativo');
+        senha.classList.add('ativo');
+        senha.disabled = false;
+    } else {
+        senha.classList.remove('ativo');
+        senha.classList.add('inativo');
+        senha.disabled = true;
+    }
+});
